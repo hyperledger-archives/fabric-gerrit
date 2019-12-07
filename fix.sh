c11d827d87906a4036a9ac11d9073da089840620
Brett Logan <Brett.T.Logan@ibm.com>
2019-12-07 00:33:24 -0500

    Format code
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>

--- fix.sh
+++ fix.sh
@@ -9,15 +9,15 @@ set -euo pipefail
 cd backup
 dirs=$(ls -d -- */)
 for dir in ${dirs}; do
-    cd ${dir}
-    rm -rf *.json*.o*
-    files=$(ls)
-    for file in ${files}; do
-        sed -i '.orig' 's/^}/},/g' ${file}
-        sed -i '.orig' '1 s/{/[{/g' ${file}
-        sed -i '.orig' '$ s/},/}]/g' ${file}
-    done
-    rm -rf *.json*.o*
-    cd ..
+  cd ${dir}
+  rm -rf *.json*.o*
+  files=$(ls)
+  for file in ${files}; do
+    sed -i '.orig' 's/^}/},/g' ${file}
+    sed -i '.orig' '1 s/{/[{/g' ${file}
+    sed -i '.orig' '$ s/},/}]/g' ${file}
+  done
+  rm -rf *.json*.o*
+  cd ..
 done
 cd ..
